f4eaea4adf92cea839aa6632ea2b06063ee24a10
Kabir-Rajbhandari <rajbhandarikabir814@gmail.com>
2026-09-22 10:30:45 +0545

    data types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,22 @@
+// Data Types in js
+
+
+/*
+    integer => number = 2 to the power 53 
+            => bigInt = huge number
+    string => " "
+    boolean => true / false
+    null => standalone value / null is object
+    undefined => value is not assigned yet
+    symbol => unique
+
+    object
+*/
+
+let name = "qwerty";
+let age = 25;
+let isWorker = true;
+
+console.log (typeof(isWorker)); // => boolean
+console.log(typeof null); // => object
+console.log(typeof undefined); // => undefined
